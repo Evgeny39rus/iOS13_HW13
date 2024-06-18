de6f00bf6e83b91df2164cddd37c8c3eedccd351
Evgeny Sushkov <evgeny.sushkov@apollointernet.io>
2024-06-18 22:57:38 +0200

    start project

---
+++ iOS-13_HW-13/iOS-13_HW-13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-13_HW-13
+//
+//  Created by Евгений Сушков on 18.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
