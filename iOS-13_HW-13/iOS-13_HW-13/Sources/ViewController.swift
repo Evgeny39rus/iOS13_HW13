5b77c6bf2008c76f4afd709162a46379a8919b50
Evgeny Sushkov <evgeny.sushkov@apollointernet.io>
2024-06-19 21:08:21 +0200

    add new button and new color rules

--- iOS-13_HW-13/iOS-13_HW-13/Sources/ViewController.swift
+++ iOS-13_HW-13/iOS-13_HW-13/Sources/ViewController.swift
@@ -7,7 +7,14 @@ enum SettingType {
     case wifi
     case bluetooth
     case cellular
+    case modem
+    case vpn
     case notifications
+    case sounds
+    case doNotDisturb
+    case screenTime
+    case general
+    case controlPoint
 }
 
 // Struct for holding the details of each setting
@@ -28,12 +35,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return tableView
     }()
     
-    private var settings: [Setting] = [
-        Setting(title: "Авиарежим", type: .airplaneMode, icon: UIImage(systemName: "airplane"), isSwitch: true),
-        Setting(title: "Wi-Fi", type: .wifi, icon: UIImage(systemName: "wifi"), isSwitch: false),
-        Setting(title: "Bluetooth", type: .bluetooth, icon: UIImage(systemName: "bluetooth"), isSwitch: false),
-        Setting(title: "Сотовая связь", type: .cellular, icon: UIImage(systemName: "antenna.radiowaves.left.and.right"), isSwitch: false),
-        Setting(title: "Уведомления", type: .notifications, icon: UIImage(systemName: "bell.badge"), isSwitch: true)
+    private var settings: [[Setting]] = [
+        [
+            Setting(title: "Авиарежим", type: .airplaneMode, icon: UIImage(systemName: "airplane")?.withTintColor(.systemOrange, renderingMode: .alwaysOriginal), isSwitch: true),
+            Setting(title: "Wi-Fi", type: .wifi, icon: UIImage(systemName: "wifi")?.withTintColor(.systemBlue, renderingMode: .alwaysOriginal), isSwitch: false),
+            Setting(title: "Bluetooth", type: .bluetooth, icon: UIImage(systemName: "bluetooth")?.withTintColor(.systemBlue, renderingMode: .alwaysOriginal), isSwitch: false),
+            Setting(title: "Сотовая связь", type: .cellular, icon: UIImage(systemName: "antenna.radiowaves.left.and.right")?.withTintColor(.systemGreen, renderingMode: .alwaysOriginal), isSwitch: false)
+        ],
+        [
+            Setting(title: "Режим модема", type: .modem, icon: UIImage(systemName: "personalhotspot")?.withTintColor(.systemGreen, renderingMode: .alwaysOriginal), isSwitch: true),
+            Setting(title: "VPN", type: .vpn, icon: UIImage(systemName: "network")?.withTintColor(.systemBlue, renderingMode: .alwaysOriginal), isSwitch: false)
+        ],
+        [
+            Setting(title: "Уведомления", type: .notifications, icon: UIImage(systemName: "bell.badge")?.withTintColor(.systemRed, renderingMode: .alwaysOriginal), isSwitch: true),
+            Setting(title: "Звуки, тактильные сигналы", type: .sounds, icon: UIImage(systemName: "speaker.wave.3.fill")?.withTintColor(.systemRed, renderingMode: .alwaysOriginal), isSwitch: false),
+            Setting(title: "Не беспокоить", type: .doNotDisturb, icon: UIImage(systemName: "moon.fill")?.withTintColor(.systemIndigo, renderingMode: .alwaysOriginal), isSwitch: true),
+            Setting(title: "Экранное время", type: .screenTime, icon: UIImage(systemName: "hourglass")?.withTintColor(.systemIndigo, renderingMode: .alwaysOriginal), isSwitch: false)
+        ],
+        [
+            Setting(title: "Основные", type: .general, icon: UIImage(systemName: "gear")?.withTintColor(.systemGray, renderingMode: .alwaysOriginal), isSwitch: false),
+            Setting(title: "Пункт управления", type: .controlPoint, icon: UIImage(systemName: "slider.horizontal.3")?.withTintColor(.systemGray, renderingMode: .alwaysOriginal), isSwitch: false)
+        ]
     ]
     
     override func viewDidLoad() {
@@ -61,17 +83,21 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     private func setupLayout() {
         tableView.snp.makeConstraints { make in
-            make.top.right.bottom.left.equalTo(view)
+            make.top.right.bottom.left.equalToSuperview()
         }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         return settings.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings[section].count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath)
-        let setting = settings[indexPath.row]
+        let setting = settings[indexPath.section][indexPath.row]
         cell.textLabel?.text = setting.title
         cell.imageView?.image = setting.icon
         cell.accessoryView = nil
@@ -79,7 +105,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         if setting.isSwitch {
             let switchControl = UISwitch()
-            switchControl.tag = indexPath.row
+            switchControl.tag = indexPath.section * 100 + indexPath.row
             switchControl.addTarget(self, action: #selector(switchChanged(_:)), for: .valueChanged)
             cell.accessoryView = switchControl
         } else {
@@ -89,17 +115,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
     
     @objc func switchChanged(_ sender: UISwitch) {
-        let setting = settings[sender.tag]
+        let section = sender.tag / 100
+        let row = sender.tag % 100
+        let setting = settings[section][row]
         print("\(setting.title) is now \(sender.isOn ? "ON" : "OFF")")
     }
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let setting = settings[indexPath.row]
-        if !setting.isSwitch {
-            // Handle navigation to detail settings screen
-            print("Navigate to settings detail for \(setting.title)")
-        }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.systemGroupedBackground
+        return headerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
     }
 }
 
