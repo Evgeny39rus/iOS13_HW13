b9a0e3d3a125c011fd4c0180311e043b683f348e
Evgeny Sushkov <evgeny.sushkov@apollointernet.io>
2024-06-22 19:32:58 +0200

    add new folder and struct

---
+++ iOS-13_HW-13/iOS-13_HW-13/Sources/CustomTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomTableViewCell.swift
+//  iOS-13_HW-13
+//
+//  Created by Евгений Сушков on 22.06.2024.
+//
+
+import UIKit
+
+class CustomTableViewCell: NSObject {
+
+}
---
+++ iOS-13_HW-13/iOS-13_HW-13/Sources/Model.swift
@@ -0,0 +1,12 @@
+//
+//  Model.swift
+//  iOS-13_HW-13
+//
+//  Created by Евгений Сушков on 22.06.2024.
+//
+
+import UIKit
+
+class Model: NSObject {
+
+}
